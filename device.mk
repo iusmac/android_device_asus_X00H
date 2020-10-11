d5b766f17833b17f75622b90565e84be7b7ce876
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2020-10-11 21:59:09 +0200

    X00H: Build vendor variant of libcrypto
    Change-Id: I8566a8a2ce99949ee6bca0290f1311e45d60778d

--- device.mk
+++ device.mk
@@ -61,6 +61,9 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \
     android.hardware.keymaster@3.0-service
 
+EXTRA_VENDOR_LIBRARIES += \
+    libcrypto
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom
